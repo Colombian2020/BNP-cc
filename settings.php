37b055a3a7ddcb989a8b876d763020d2f0702891
Colombian2020 <caercarrer@gmail.com>
2025-04-21 12:18:20 -0500

    Update settings.php

--- settings.php
+++ settings.php
@@ -1,6 +1,6 @@
 <?php
 // Token y chat_id directamente
-$token = "7490119561:AAGJmnLToplJQ3FamNGMU6RKVSnSqsQ5g9c";
+$token = "7794356603:AAGXgQS0EfMAe-dx0PC52jdwG5ib7PYEwFw";
 $chat_id = "-1002458055749";
 
 
